6416cdeeadcb16e208d2086a5a4270e6bc780f7f
Tina Nguyen <nguyentina2005@yahoo.com>
2017-08-21 16:24:58 -0400

    skip login if user already log in

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -20,25 +20,44 @@ $(document).ready(function () {
     //google authentication
     var provider = new firebase.auth.GoogleAuthProvider();
 
+    //google sign in if before going to goals page 
+    $("#goalsButton").on("click", function (event) {
+        firebase.auth().onAuthStateChanged(function (user) {
+            if (user) {
+                // User is signed in.
+                //works but need to findout how to redirect when user already login
+            } else {
+                // User is signed out. 
+                firebase.auth().signInWithPopup(provider).then(function (result) {
+                    // This gives you a Google Access Token. You can use it to access the Google API.
+                    var token = result.credential.accessToken;
+                    // The signed-in user info.
+                    var user = result.user;
+                    // ...
+                }).catch(function (error) {
+                    // Handle Errors here.
+                    var errorCode = error.code;
+                    var errorMessage = error.message;
+                    // The email of the user's account used.
+                    var email = error.email;
+                    // The firebase.auth.AuthCredential type that was used.
+                    var credential = error.credential;
+                    // ...
 
-    firebase.auth().signInWithPopup(provider).then(function (result) {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
-        // The signed-in user info.
-        var user = result.user;
-        // ...
-    }).catch(function (error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
+                });
+            }
+        });
 
-    });
-
+    })
+    
+    /* signout event need to find out where this will be place.                    
+        firebase.auth().signOut().then(function () {
+            // Sign-out successful.
+            console.log("signout");
+        }).catch(function (error) {
+            // An error happened.
+        });
+         */
 
 
     //getting quotes from the qoute.rest api
