1dcfb6a5acca6f3b574280958cf1936ecff687ed
bernardw01 <bernard.l.williams@live.com>
2017-08-24 22:10:19 -0400

    Completed UI additions to show the goals for the signed in user. Added delete functionality to remove goals from the database.

--- routes/cnt_goal.js
+++ routes/cnt_goal.js
@@ -68,9 +68,11 @@ router.post("/update", function (req, res) {
 });
 
 router.post("/del", function (req, res) {
+
+    console.log('Delete: ' + JSON.stringify(req.body));
     db.Goal.destroy({
         where: {
-            UserUserID: parseInt(req.body.userID)
+            goalID: parseInt(req.body.goalID)
         }
     }).then(function () {
         res.redirect("/");
