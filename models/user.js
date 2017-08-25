839af13374f002a26ecfe7efd92a45468da2f781
Melissa <melissarenaut@gmail.com>
2017-08-25 01:40:49 -0400

    create user form now connected

--- models/user.js
+++ models/user.js
@@ -27,36 +27,3 @@ module.exports = function (sequelize, DataTypes) {
 
 
 
-///MY ATTEMPT TO DO WHAT BERNARD ASKED ME//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//ReferenceError: $ is not defined
-//added ajax script link after jquery but still getting this error
-
-//
-//$(document).ready(function () {
-//
-//
-//
-//			/* attach a on click event to the form */
-//			$("#createUserSubmit").on("click", function (event) {
-//
-//					/* stop form from submitting normally */
-//					event.preventDefault();
-//
-//					/* get the action attribute from the <form element */
-//					var email = $("#emailInput");
-//					var DOB = $("#nameInput");
-//					var fullName = $("#nameInput");
-//
-//					$.post("index.hdb", {
-//						email: email,
-//						DOB: DOB,
-//						fullName: fullName
-//					}).
-//
-//					done(function (data) {
-//							console.log(data);
-//
-//						});
-//					});
-//
-//			});
